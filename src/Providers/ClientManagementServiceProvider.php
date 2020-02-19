006e57871f0869612fd0e7c75e9a571a8d6972f2
gabbydgab <codingmattersio@gmail.com>
2020-02-19 13:29:05 +0800

    intial structure

---
+++ src/Providers/ClientManagementServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodingMatters\ClientManagement\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class ClientManagementServiceProvider extends ServiceProvider
+{
+    public function boot() : void
+    {
+        //
+    }
+
+    public function register() : void
+    {
+        //
+    }
+}
