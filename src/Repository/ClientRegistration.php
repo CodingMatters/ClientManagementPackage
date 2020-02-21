307e92f67daef23267b2dd475f13a4a2520697f3
gabbydgab <codingmattersio@gmail.com>
2020-02-21 15:47:13 +0800

    updated implementation

--- src/Repository/ClientRegistration.php
+++ src/Repository/ClientRegistration.php
@@ -7,10 +7,10 @@ use GuzzleHttp\Client;
 
 final class ClientRegistration
 {
-    public static function save(string $username, string $first_name)
+    public static function save(string $uuid, string $username, string $firstName, string $lastName)
     {
         $client = new Client(['base_uri' => config('codingmatters.crm.url')]);
-        $response = $client->post('/sample', ['name' => 'gab']);
+        $response = $client->post('/register', ['first_name' => $firstName, 'last_name' => $lastName, 'code' =>$uuid, 'username' => $username]);
         dd($response);
     }
 }
