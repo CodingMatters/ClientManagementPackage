39f70c16f0d84402f4679ed1d9a0fbe8b9afd403
gabbydgab <codingmattersio@gmail.com>
2020-02-21 15:24:37 +0800

    updates

--- src/Repository/ClientRegistration.php
+++ src/Repository/ClientRegistration.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 namespace CodingMatters\ClientManagement\Repository;
+use GuzzleHttp\Client;
 
 final class ClientRegistration
 {
     public static function save(string $username, string $first_name)
     {
-        dd(config('codingmatters.crm.url'));
+        $client = new Client(['base_uri' => config('codingmatters.crm.url')]);
+        $response = $client->post('/sample', ['name' => 'gab']);
+        dd($response);
     }
 }
