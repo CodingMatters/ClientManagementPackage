6d47e546c84277090480a7ed218ae8f26ebf46b0
gabbydgab <codingmattersio@gmail.com>
2020-02-21 16:30:59 +0800

    updates

--- src/Repository/ClientRegistration.php
+++ src/Repository/ClientRegistration.php
@@ -9,8 +9,8 @@ final class ClientRegistration
 {
     public static function save(string $uuid, string $username, string $firstName, string $lastName)
     {
-        $client = new Client(['base_uri' => config('codingmatters.crm.url')]);
-        $response = $client->post('/register', ['first_name' => $firstName, 'last_name' => $lastName, 'code' =>$uuid, 'username' => $username]);
+        $client = new Client();
+        $response = $client->post(config('codingmatters.crm.url') . '/register', ['first_name' => $firstName, 'last_name' => $lastName, 'code' =>$uuid, 'username' => $username]);
         dd($response);
     }
 }
--- tests/ClientRegistrationTest.php
+++ tests/ClientRegistrationTest.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace CodingMatters\ClientManagementTests;
 
+use CodingMatters\ClientManagement\Repository\ClientRegistration;
+use Illuminate\Foundation\Testing\WithFaker;
+
 final class ClientRegistrationTest extends CodingMattersTestCase
 {
+    use WithFaker;
+
     /**
      * @test
      */
