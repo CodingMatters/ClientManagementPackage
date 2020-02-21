2f7e525b3bd93e53745c9952473708cfceb52be8
gabbydgab <codingmattersio@gmail.com>
2020-02-21 14:53:54 +0800

    fixes index name

--- config/codingmatters.php
+++ config/codingmatters.php
@@ -2,7 +2,6 @@
 
 return [
     'client-service' => [
-        'url' => env("CODINGMATTERS_CRM_SERVICE_URL", 'https://crm.local'),
-        'token' => env("CODINGMATTERS_CRM_SERVICE_TOKEN", "your-token-here"),
+        'url' => env("CODINGMATTERS_CRM_URL", 'https://crm.local'),
     ],
 ];
